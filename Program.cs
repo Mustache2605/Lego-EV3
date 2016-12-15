45352739ae7339201ea5e56cef329fc78af4a5cd
Nyma Dolatkhahnejad <nyma@telfort.nl>
2016-12-15 12:11:45 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 
 namespace soundSharp {
     class mainClass {
-        /* Integer: Usage for the attempt check system */
+
+        /* Integers: Usage for the attempt check system */
         public static int attempt = 1;
         public static int maxAttempts = 3;
 
-        /* List: Creates a list with items */
-        public static List<MPlayers> MPlist = new List<MPlayers> {
-        new MPlayers(1, 500, "GET technologies .inc", "HF 410", 4096, 129.95),
-        new MPlayers(2, 500, "Far & Loud", "XM 600", 8192, 224.95),
-        new MPlayers(3, 500, "Innotivative", "Z3", 512, 79.95),
-        new MPlayers(4, 500, "Resistance S.A", "3001", 4096, 124.95),
-        new MPlayers(5, 500, "CBA", "NXT volume", 2048, 159.05),
-        };
+        /* string: Returns name into the whole system */
+        static string name;
 
         /* Easter egg: It's ya boi easter egg muhahaha... */
         private static void easterEgg() {
@@ -111,13 +106,13 @@ namespace soundSharp {
         }
 
         /* Function: Main, if user is logged in show menu and so on */
-        static void Main(string[] args) {
+        public static void Main(string[] args) {
             Console.Title = "SoundSharp";
             Console.WriteLine("Vul je naam in:");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
 
             Boolean isLoggedIn;
-            isLoggedIn = logIn(name);
+            isLoggedIn = logIn();
             if (isLoggedIn == true) {
                 showMenu();
                 readKey();
@@ -128,7 +123,7 @@ namespace soundSharp {
         }
 
         /* Function: Login (name, attempts and password check) */
-        static Boolean logIn(string name) {
+        static Boolean logIn() {
             Console.WriteLine("Hallo chef {0}, vul je wachtwoord in:", name);
             string input;
 
@@ -149,9 +144,12 @@ namespace soundSharp {
         }
 
         /* Function: Shows the menu */
-        static void showMenu() {
-            Console.Beep();
-            Console.WriteLine("Welkom bij het SoundSharp hoofdmenu!");
+        public static void showMenu() {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("#######################################################################################################################");
+            Console.WriteLine("#################################### Welkom bij het SoundSharp hoofdmenu, {0}! ####################################", name);
+            Console.WriteLine("#######################################################################################################################");
+            Console.ResetColor();
             Console.WriteLine("1. MP3 spelers bekijken");
             Console.WriteLine("2. Voorraad bekijken");
             Console.WriteLine("3. Voorraad muteren");
@@ -163,7 +161,7 @@ namespace soundSharp {
             Console.WriteLine("9. Programma verlaten");
         }
 
-        /* Function: Reads the key for the menu function described as above and goes to the function. Ex. easteregg */
+        /* Function: Reads the key for the menu function described as above and goes to the function that is assigned to the key */
         static void readKey() {
             while (true) {
                 ConsoleKeyInfo key = Console.ReadKey();
@@ -186,27 +184,27 @@ namespace soundSharp {
 
                     case '3':
                         Console.Clear();
-                        Console.WriteLine("Menu item 3!");
+                        changeStock();
                         break;
 
                     case '4':
                         Console.Clear();
-                        Console.WriteLine("Menu item 4!");
+                        showStatistics();
                         break;
 
                     case '5':
                         Console.Clear();
-                        Console.WriteLine("Menu item 5!");
+                        addMpPlayer();
                         break;
 
                     case '6':
                         Console.Clear();
-                        Console.WriteLine("Menu item 6!");
+                        Console.WriteLine("Dit doet niks, maar je kan het proberen...");
                         break;
 
                     case '7':
                         Console.Clear();
-                        Console.WriteLine("Menu item 7!");
+                        Console.WriteLine("Dit doet niks, maar je kan het proberen...");
                         break;
 
                     case '8':
@@ -227,14 +225,26 @@ namespace soundSharp {
             }
         }
 
+        /* List: Creates a list with items */
+        public static List<MPlayers> MPlist = new List<MPlayers> {
+        new MPlayers(1, 500, "GET technologies .inc", "HF 410", 4096, 129.95),
+        new MPlayers(2, 500, "Far & Loud", "XM 600", 8192, 224.95),
+        new MPlayers(3, 500, "Innotivative", "Z3", 512, 79.95),
+        new MPlayers(4, 500, "Resistance S.A", "3001", 4096, 124.95),
+        new MPlayers(5, 500, "CBA", "NXT volume", 2048, 159.05),
+        };
+
         /* Function: Shows information from the list about the MP3 players */
         public static void MPShowList() {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("MP3 spelers die in het systeem staan:");
+            Console.ResetColor();
             foreach (MPlayers item in MPlist) {
-                Console.WriteLine("ID: " + item.PlayerID + "\nVoorraad: " + item.PlayerStock + " stuks\nMerk: " + item.PlayerMake + "\nModel: " + item.PlayerModel + "\nGrootte:" + item.PlayerMB + " MB \nPrijs: " + item.PlayerPrice + " euro \n");
+                Console.WriteLine("ID: " + item.PlayerID + "\nVoorraad: " + item.PlayerStock + " stuks\nMerk: " + item.PlayerMake + "\nModel: " + item.PlayerModel + "\nGrootte: " + item.PlayerMB + " MB \nPrijs: " + item.PlayerPrice + " euro \n");
             }
         }
 
-        /* Struct: Seems like a struct... lol */
+        /* Struct: Well... as the name says a struct */
         public struct MPlayers {
             public int PlayerID;
             public int PlayerStock;
@@ -243,7 +253,7 @@ namespace soundSharp {
             public float PlayerMB;
             public double PlayerPrice;
 
-            /* Reads the struct I think...??? */
+            /* Constructor: Reads the struct and give it's directions */
             public MPlayers(int PlayerID, int PlayerStock, String PlayerMake, String PlayerModel, float PlayerMB, double PlayerPrice) {
                 this.PlayerID = PlayerID;
                 this.PlayerStock = PlayerStock;
@@ -260,6 +270,23 @@ namespace soundSharp {
             foreach (MPlayers item in MPlist) {
                 Console.WriteLine("ID: " + item.PlayerID + "\nVoorraad: " + item.PlayerStock + " stuks\n");
             }
+        }
+
+        /* Function: Change the stock of available MP players */
+        public static void changeStock() {
+
+        }
+
+        /* Function: Add a MP player to the system */
+        public static void addMpPlayer() {
+            /* Adds MP player function here */
+        }
+
+        /* Function: Show the statistics of all the available MP players ex. average of all MP players */
+        public static void showStatistics() {
+            Console.WriteLine("Totale statistieken:");
+            
+
         }
     }
 }
